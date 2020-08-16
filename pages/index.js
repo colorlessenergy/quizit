d3a750fcc8cd335f4ad188a422c4eea1f57c8623
colorlessenergy <brianmunozdev@gmail.com>
2020-08-16 12:54:02 -0700

    create array of objects with questions and display on home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({ questions }) {
+  let displayQuestions = questions.map(function ({question, answer}, index) {
+    return (
+      <div key={index}>
+        <h3>{question}</h3>
+        <p>{answer}</p>
+      </div>
+    )
+  })
   return (
     <div>
       <Head>
@@ -11,6 +19,30 @@ export default function Home() {
       <h1>
         quiz it
       </h1>
+
+      <div>
+        {displayQuestions}
+      </div>
     </div>
   )
+}
+
+// This function gets called at build time
+export function getStaticProps() {
+  const questions = [
+    {
+      question: 'What is the CSS box model?',
+      answer: 'Every element is made up of Margin, Border, Padding, and Content'
+    },
+    {
+      question: 'What is box-sizing: border-box',
+      answer: 'when assigning a width or height it will include the padding and border in that calculation'
+    }
+  ]
+
+  return {
+    props: {
+      questions
+    },
+  }
 }
