36321a4bb58714e3fb823b9e6ae410eec8365a85
colorlessenergy <brianmunozdev@gmail.com>
2020-09-02 12:43:40 -0700

    move the head and everything inside into the return statement

--- pages/_app.js
+++ pages/_app.js
@@ -3,25 +3,30 @@ import Head from 'next/head';
 
 
 function MyApp({ Component, pageProps }) {
-  <Head>
-    <link href = "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
-    rel = "stylesheet"/>
-    <meta charset='utf-8' />
-    <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-    <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
-    <meta name='description' content='quizit - technical questions' />
-    <meta name='application-name' content='quizit' />
-    <meta name='apple-mobile-web-app-capable' content='yes' />
-    <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-    <meta name='apple-mobile-web-app-title' content='quizit' />
+  return (
+    <div>
+      <Head>
+        <link href = "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+        rel = "stylesheet"/>
+        <meta charset='utf-8' />
+        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+        <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+        <meta name='description' content='quizit - technical questions' />
+        <meta name='application-name' content='quizit' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+        <meta name='apple-mobile-web-app-title' content='quizit' />
 
-    <link rel="manifest" href="/manifest.json" />
-    <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
-    <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-    <meta name="theme-color" content="#317EFB"/>
-  </Head>
-  return <Component {...pageProps} />
+        <link rel="manifest" href="./manifest.json" />
+
+        <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+        <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="theme-color" content="#000000"/>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+    )
 }
 
 export default MyApp
